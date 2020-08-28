11d0884783e66cae5ca4dbe42fef20ea6770d0c5
hp <hritik.ranjan_cs19@gla.ac.in>
2020-08-28 20:19:34 +0530

    created getter and setter for Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,5 +1,5 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+ *  User: Hritik Ranjan(191500352)
  *  Date: 21/08/20
  *  Time: 3:50 PM
  *  File Name : Library.java
@@ -9,4 +9,12 @@ package definitions;
 public class Library {
     //fields
     private Book[] booksAvailable;
+
+    //getter and setter
+    public Book[] getBooksAvailable() {
+        return booksAvailable.clone();
+    }
+    public void setBooksAvailable(Book[] booksAvailable) {
+        this.booksAvailable = booksAvailable;
+    }
 }
